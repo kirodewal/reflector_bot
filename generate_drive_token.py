ae78fd01883eba25617fde627937f26a5a67a36c
Kamlesh Kirodewal <kkirodewal@gmail.com>
2020-11-15 21:40:44 +0530

    Create generate_drive_token.py

--- generate_drive_token.py
+++ generate_drive_token.py
@@ -4,7 +4,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 credentials = None
-__G_DRIVE_TOKEN_FILE = "token.pickle"
+__G_DRIVE_TOKEN_FILE = "ya29.A0AfH6SMCzFm3emks1TKDz5DwBFSmsTHe9PMU232ncRIKb4x2gd1lDy3qnb9C5toN8lLUKnpcLFwwSLaUUDw4l7Y-Rce2zioLDpEwt7LpxUE3wigVRWHpdMM7DoMvX6oTKVQpGYiTwvMmgmuY0cv4fA8qrPIVLfueh5rlgclkBlY0"
 __OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive"]
 if os.path.exists(__G_DRIVE_TOKEN_FILE):
     with open(__G_DRIVE_TOKEN_FILE, 'rb') as f:
